3949cc282e64bf05616cf5409f836072bbeb77a0
Sayyed Masud <84665073+syedsm@users.noreply.github.com>
2025-02-05 21:37:41 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,8 @@ const Paypal = require('./routers/paypal.router')
 const cors = require("cors")
 
 const corsOptions = {
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+    origin:"https://inventorymanagmentsystembackend.onrender.com",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 };
