07471b30fb406f8daa0572431a6a39a3be4c5fb4
Sayyed Masud <84665073+syedsm@users.noreply.github.com>
2025-02-06 16:06:26 +0530

    Update Login.jsx

--- frontend/src/Auth/Login.jsx
+++ frontend/src/Auth/Login.jsx
@@ -38,9 +38,9 @@ function Login() {
         "https://inventorymanagmentsystembackend.onrender.com/api/auth/login",
         data
       );
-      console.log("data recived", res);
+      // console.log("data recived", res);
       const decodedTokendata = decodeToken(res.data.token);
-      console.log("Decoded token:", decodedTokendata);
+      // console.log("Decoded token:", decodedTokendata);
       localStorage.setItem("loginemail", decodedTokendata.email);
       localStorage.setItem("authToken", decodedTokendata.token);
       localStorage.setItem("userRole", decodedTokendata.role);
@@ -72,7 +72,7 @@ function Login() {
     try {
       const result = await signInWithPopup(auth, new GoogleAuthProvider());
       const user = result.user;
-      console.log("data recived", user);
+      // console.log("data recived", user);
 
       // Call the backend API with the Google access token
       const res = await axios.post(
@@ -82,7 +82,7 @@ function Login() {
         }
       );
 
-      console.log("data recived",res);
+      // console.log("data recived",res);
 
       if (res.status === 200 || res.status === 201) {
         const decodedTokendata = decodeToken(res.data.token);
