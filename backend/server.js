80d3edc8378d1a9866afccae81c200b8a716ca30
Syed Masood <masoodsyed123786@gmail.com>
2025-02-06 12:41:41 +0530

    fix url

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ const cors = require("cors")
 
 const corsOptions = {
     // origin: "http://localhost:5173",
-    origin:"https://inventorymanagmentsystembackend.onrender.com",
+    origin:"https://stockmanagementapp.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 };
